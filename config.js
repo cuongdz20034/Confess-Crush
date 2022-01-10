904ab0529d56405191865e05f3d0856e25c3e501
cuongdz20034 <97436870+cuongdz20034@users.noreply.github.com>
2022-01-10 16:08:03 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,7 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    titleWeb: "GỬI CẬU LÊ NGỌC ANH <3",
+    introTitle: 'Lê Ngọc Anh ',
+    introDesc: `Giá như anh gặp em sớm hơn thì bài văn tả thiên thần của anh đã không bỏ giấy trắng ,
     btnIntro: '^^HiHi^^',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
